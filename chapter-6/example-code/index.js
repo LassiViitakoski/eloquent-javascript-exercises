b9c574ceabc3f1cd1c464a843bebe29a857e80b0
N3825 <N3825@student.jamk.fi>
2021-05-18 18:39:11 +0300

    chapter-6 exercise1 done

--- chapter-6/example-code/index.js
+++ chapter-6/example-code/index.js
@@ -120,10 +120,10 @@ class Rabbit {
 let killerRabbit = new Rabbit("killer");
 let blackRabbit = new Rabbit("black");
 
-killerRabbit.speak("RHIIIII!");
+/*killerRabbit.speak("RHIIIII!");*/
 
 let object = new class { getWord() { return "hello"; } };
-console.log(object.getWord());
+/*console.log(object.getWord());*/
 
 
 
@@ -132,9 +132,11 @@ console.log(object.getWord());
 
 
 Rabbit.prototype.teeth = "small";
-console.log(killerRabbit.teeth);
+/*console.log(killerRabbit.teeth);*/
 
 killerRabbit.teeth = "long, sharp, and bloody";
+
+/*
 console.log(killerRabbit.teeth);
 console.log(blackRabbit.teeth);
 console.log(Rabbit.prototype.teeth);
@@ -142,7 +144,224 @@ console.log(Rabbit.prototype.teeth);
 console.log(Array.prototype.toString == Object.prototype.toString);
 console.log([1, 2].toString());
 console.log(Object.prototype.toString.call([1, 2]));
+*/
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // MAPS
+
+
+/*
+let ages = {
+    Boris: 39,
+    Liang: 22,
+    Julia: 62
+};
+
+console.log(`Julia is ${ages["Julia"]}`);
+console.log("Is Jack's age known?", "Jack" in ages);
+console.log("Is toString's age known", "toString" in ages);
+console.log("toString" in Object.create(null));
+*/
+
+let ages = new Map();
+ages.set("Boris", 39);
+ages.set("Liang", 22);
+ages.set("Julia", 62);
+
+/*
+console.log(`Julia is ${ages.get("Julia")}`);
+console.log("Is Jack's age known?", ages.has("Jack"));
+console.log(ages.has("toString"));
+
+console.log({ x: 1 }.hasOwnProperty("x"));
+console.log({ x: 1 }.hasOwnProperty("toString"));
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// POLYMORPHISM
+
+
+Rabbit.prototype.toString = function() {
+    return `a ${this.type} rabbit`;
+};
+
+/*console.log(String(blackRabbit));*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// SYMBOLS
+
+
+let sym = Symbol("name");
+/*console.log(sym == Symbol("name"));*/
+
+Rabbit.prototype[sym] = 55;
+/*console.log(blackRabbit[sym]);*/
+
+const toStringSymbol = Symbol("toString");
+
+Array.prototype[toStringSymbol] = function() {
+    return `${this.length} cm of blue yarn`;
+};
+
+/*
+console.log([1, 2].toString());
+console.log([1, 2][toStringSymbol]());
+*/
+
+let stringObject = {
+    [toStringSymbol]() { return "a jute rope"; }
+};
+
+/*console.log(stringObject[toStringSymbol]());*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// THE ITERATOR INTERFACE
+
+let okIterator = "OK"[Symbol.iterator]();
+
+/*
+console.log(okIterator.next());
+console.log(okIterator.next());
+console.log(okIterator.next());
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+
+class Matrix {
+    constructor(width, height, element = (x, y) => undefined) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.content[y * width + x] = element(x, y);
+            }
+        }
+    }
+
+    get(x, y) {
+        return this.content[y * this.width + x];
+    }
+
+    set(x, y, value) {
+        this.content[y * this.width + x] = value;
+    }
+}
+
+class MatrixIterator {
+    constructor(matrix) {
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+
+    next() {
+        if (this.y == this.matrix.height) return { done: true };
+
+        let value = { 
+            x: this.x,
+            y: this.y,
+            value: this.matrix.get(this.x, this.y)
+        };
+
+        this.x++;
+
+        if (this.x == this.matrix.height) {
+            this.x = 0;
+            this.y++;
+        }
+
+        return { value, done: false };
+    }
+}
+
+Matrix.prototype[Symbol.iterator] = function() {
+    return new MatrixIterator(this);
+}
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+
+for (let {x, y, value} of matrix) {
+    /*console.log(x, y, value);*/
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// GETTERS, SETTERS AND STATICS
+
+
+let varyingSize = {
+    get size() {
+        return Math.floor(Math.random() * 100);
+    }
+};
+
+/*console.log(varyingSize.size);*/
+
+class Temperature {
+    constructor(celsius) {
+        this.celsius = celsius;
+    }
+
+    get fahrenheit() {
+        return this.celsius * 1.8 + 32;
+    }
+
+    set fahrenheit(value) {
+        this.celsius = (value - 32) / 1.8;
+    }
+
+    static fromFahrenheit(value) {
+        return new Temperature((value - 32) / 1.8);
+    }
+}
+
+let temp = new Temperature(22);
+/*console.log(temp.fahrenheit);*/
+temp.fahrenheit = 86;
+/*console.log(temp);*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// INHERITANCE
+
+
+class SymmetricMatrix extends Matrix {
+    constructor(size, element = (x, y) => undefined) {
+        super(size, size, (x, y) => {
+            if (x < y) return element(y, x);
+            else return element(x, y);
+        });
+    }
+
+    set(x, y, value) {
+        super.set(x, y, value);
+
+        if (x != y) {
+            super.set(y, x, value);
+        }
+    }
+}
+
+let symmetricMatrix = new SymmetricMatrix(5, (x, y) => `${x}, ${y}`);
+/*console.log(symmetricMatrix.get(2, 3));*/
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+// THE INSTANCEOF OPERATOR
+
+
+/*
+console.log(new SymmetricMatrix(2) instanceof SymmetricMatrix);
+console.log(new SymmetricMatrix(2) instanceof Matrix);
+console.log(new Matrix(2, 2) instanceof SymmetricMatrix);
+console.log([1] instanceof Array);
+*/
---
+++ chapter-6/exercise1/index.js
@@ -0,0 +1,51 @@
+// OMA RATKAISU
+
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get length() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    plus(vector) {
+        let newX = this.x + vector.x;
+        let newY = this.y + vector.y;
+
+        return new Vector(newX, newY);
+    }
+
+    minus(vector) {
+        let newX = this.x - vector.x;
+        let newY = this.y - vector.y;
+
+        return new Vector(newX, newY);
+    }
+}
+
+
+
+// MALLIRATKAISU
+
+/*
+class Vec {
+    constructor(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    plus(other) {
+      return new Vec(this.x + other.x, this.y + other.y);
+    }
+  
+    minus(other) {
+      return new Vec(this.x - other.x, this.y - other.y);
+    }
+  
+    get length() {
+      return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+}
+*/
